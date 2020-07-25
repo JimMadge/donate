72e91efdf3d1a355c881269d63a98e9eb5fea876
Jim Madge <jmmadge@gmail.com>
2020-07-25 16:26:28 +0100

    Add test for reading ledger file

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import donate
 from donate.ledger import _get_ledger
+import json
 
 
 class TestGetLedger:
@@ -14,6 +15,27 @@ class TestGetLedger:
         ledger, ledger_path = _get_ledger()
 
         assert ledger == {"total": Counter(), "number": Counter()}
-        print(ledger_path)
-        print(tmp_path)
+        assert str(ledger_path).endswith("donate/ledger.json")
+
+    def test_ledger(self, monkeypatch, tmp_path):
+        def mock_save_data_path(resource):
+            return tmp_path / resource
+
+        ledger_dict = {
+            "total": {"a": 50, "b": 30},
+            "number": {"a": 5, "b": 2}
+        }
+        ledger_path = mock_save_data_path("donate")
+        ledger_path.mkdir()
+        with open(ledger_path / "ledger.json", "w") as ledger:
+            json.dump(ledger_dict, ledger)
+
+        monkeypatch.setattr(donate.ledger.BaseDirectory, "save_data_path",
+                            mock_save_data_path)
+
+        ledger, ledger_path = _get_ledger()
+
+        assert ledger == {
+            "total": Counter(ledger_dict["total"]),
+            "number": Counter(ledger_dict["number"])}
         assert str(ledger_path).endswith("donate/ledger.json")
