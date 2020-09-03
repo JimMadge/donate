2639f97d19cf165d355c3d809f419c69330cf9af
Jim Madge <jmmadge@gmail.com>
2020-09-03 16:00:50 +0100

    Restructure ledger functions

--- donate/ledger.py
+++ donate/ledger.py
@@ -5,23 +5,26 @@ from tabulate import tabulate
 from xdg import BaseDirectory
 
 
-def _get_ledger():
-    # Open existing ledger, or create a new one
+def _ledger_path():
     data_path = Path(BaseDirectory.save_data_path("donate"))
-    ledger_path = data_path / "ledger.json"
+    return data_path / "ledger.json"
+
+
+def _get_ledger():
+    """Return the existing ledger or create an empty ledger."""
     try:
-        with open(ledger_path, "r") as ledger_file:
+        with open(_ledger_path(), "r") as ledger_file:
             ledger = json.load(ledger_file)
             ledger["total"] = Counter(ledger["total"])
             ledger["number"] = Counter(ledger["number"])
     except FileNotFoundError:
         ledger = {"total": Counter(), "number": Counter()}
 
-    return ledger, ledger_path
+    return ledger
 
 
 def update_ledger(donations, decimal_currency):
-    ledger, ledger_path = _get_ledger()
+    ledger = _get_ledger()
     total = ledger["total"]
     number = ledger["number"]
 
@@ -32,12 +35,12 @@ def update_ledger(donations, decimal_currency):
         total[name] += amount
         number[name] += 1
 
-    with open(ledger_path, "w") as ledger_file:
+    with open(_ledger_path(), "w") as ledger_file:
         json.dump(ledger, ledger_file, indent=2)
 
 
 def ledger_stats(currency_symbol):
-    ledger, ledger_path = _get_ledger()
+    ledger = _get_ledger()
     total = ledger["total"]
     number = ledger["number"]
 
--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -1,56 +1,57 @@
 from collections import Counter
 import donate
-from donate.ledger import _get_ledger, ledger_stats
+from donate.ledger import _ledger_path, _get_ledger, ledger_stats
 import json
+import pytest
 import re
 
 
-class TestGetLedger:
-    def test_no_ledger(self, monkeypatch, tmp_path):
-        def mock_save_data_path(resource):
-            return tmp_path / resource
+def test_ledger_path():
+    ledger_path = _ledger_path()
+    path_string = str(ledger_path.absolute())
 
-        monkeypatch.setattr(donate.ledger.BaseDirectory, "save_data_path",
-                            mock_save_data_path)
+    assert path_string.split("/")[-1] == "ledger.json"
+    assert path_string.split("/")[-2] == "donate"
 
-        ledger, ledger_path = _get_ledger()
 
-        assert ledger == {"total": Counter(), "number": Counter()}
-        assert str(ledger_path).endswith("donate/ledger.json")
+@pytest.fixture
+def mock_ledger_path(tmp_path):
+    def _ledger_path():
+        return tmp_path / "ledger.json"
+    return _ledger_path
 
-    def test_ledger(self, monkeypatch, tmp_path):
-        def mock_save_data_path(resource):
-            return tmp_path / resource
 
-        ledger_dict = {
-            "total": {"a": 50, "b": 30},
-            "number": {"a": 5, "b": 2}
-        }
-        ledger_path = mock_save_data_path("donate")
-        ledger_path.mkdir()
-        with open(ledger_path / "ledger.json", "w") as ledger:
-            json.dump(ledger_dict, ledger)
+def test_no_ledger(monkeypatch, mock_ledger_path):
+    monkeypatch.setattr(donate.ledger, "_ledger_path", mock_ledger_path)
 
-        monkeypatch.setattr(donate.ledger.BaseDirectory, "save_data_path",
-                            mock_save_data_path)
+    ledger = _get_ledger()
 
-        ledger, ledger_path = _get_ledger()
+    assert ledger == {"total": Counter(), "number": Counter()}
 
-        assert ledger == {
-            "total": Counter(ledger_dict["total"]),
-            "number": Counter(ledger_dict["number"])}
-        assert str(ledger_path).endswith("donate/ledger.json")
+
+def test_ledger(monkeypatch, mock_ledger_path):
+    ledger_dict = {
+        "total": {"a": 50, "b": 30},
+        "number": {"a": 5, "b": 2}
+    }
+    with open(mock_ledger_path(), "w") as ledger:
+        json.dump(ledger_dict, ledger)
+
+    monkeypatch.setattr(donate.ledger, "_ledger_path", mock_ledger_path)
+
+    ledger = _get_ledger()
+
+    assert ledger == {
+        "total": Counter(ledger_dict["total"]),
+        "number": Counter(ledger_dict["number"])}
 
 
 def test_ledger_stats(monkeypatch):
     def mock_get_ledger():
-        return (
-            {
-                "total": {"a": 50, "b": 30},
-                "number": {"a": 5, "b": 2}
-            },
-            None
-        )
+        return {
+            "total": {"a": 50, "b": 30},
+            "number": {"a": 5, "b": 2}
+        }
 
     monkeypatch.setattr(donate.ledger, "_get_ledger", mock_get_ledger)
 
