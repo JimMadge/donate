aac9edc940a1565aa770ae866758b61f03d378af
Jim Madge <jmmadge@gmail.com>
2021-04-24 16:59:52 +0100

    Add static typing for logs module

--- donate/logs.py
+++ donate/logs.py
@@ -1,24 +1,31 @@
+from .donee import Donee
+from collections import Counter
 from csv import writer
 from datetime import date
 from pathlib import Path
+from typing import Union
 from xdg import BaseDirectory  # type: ignore
 
 
-def _log_path():
+def _log_path() -> Path:
     data_path = Path(BaseDirectory.save_data_path("donate"))
     return data_path / "donation_log.csv"
 
 
-def update_log(donations, currency_symbol, decimal_currency):
+def update_log(donations: Counter[Donee], currency_symbol: str,
+               decimal_currency: bool) -> None:
     with open(_log_path(), "a", newline="") as csvfile:
         log_writer = writer(csvfile)
 
         donation_date = date.today().isoformat()
 
         for donee, amount in donations.items():
+            log_amount: Union[int, float]
             if decimal_currency:
-                amount /= 100
+                log_amount = amount / 100
+            else:
+                log_amount = amount
 
             log_writer.writerow(
-                [donation_date, donee.name, currency_symbol, amount]
+                [donation_date, donee.name, currency_symbol, log_amount]
             )
